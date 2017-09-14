67ae97654300989193032c1931055260d0e8fd35
willyheng <willy.heng@gmail.com>
2017-09-14 16:23:53 +0000

    Corrected error

--- Data_Cleaning/Assignment/run_analysis.R
+++ Data_Cleaning/Assignment/run_analysis.R
@@ -5,21 +5,29 @@ file_test_x <- "UCI HAR Dataset/test/X_test.txt"
 file_test_y <- "UCI HAR Dataset/test/y_test.txt"
 file_train_x <- "UCI HAR Dataset/train/X_train.txt"
 file_train_y <- "UCI HAR Dataset/train/y_train.txt"
+file_subject_train <- "UCI HAR Dataset/train/subject_train.txt"
+file_subject_test <- "UCI HAR Dataset/test/subject_test.txt"
 
 file_features <- "UCI HAR Dataset/features.txt"
 
-test_x <- read.table(file_test_x, header=FALSE, sep ="")
-train_x <- read.table(file_train_x, header=FALSE, sep ="")
+test_x <- read.table(file_test_x)
+train_x <- read.table(file_train_x)
 
-test_y <- read.table(file_test_y, header=FALSE, sep ="")
-train_y <- read.table(file_train_y, header=FALSE, sep ="")
+test_y <- read.table(file_test_y)
+train_y <- read.table(file_train_y)
+
+test_subject <- read.table(file_subject_test)
+train_subject <- read.table(file_subject_train)
+
 
 # 1.1. Merge data sets
-data_x_raw <- rbind(test_x, train_x) 
+data_subject <- rbind(train_subject, test_subject) %>%
+  rename(subject = V1)
+data_x_raw <- rbind(train_x, test_x) 
 unwanted <- c((303:344), (382:423), (461:502))
 data_x <- select(data_x_raw, -unwanted)
 
-data_y <- rbind(test_y, train_y)
+data_y <- rbind(train_y, test_y)
 
 # 3. Activity labels
 file_activity <- "UCI HAR Dataset/activity_labels.txt"
@@ -36,11 +44,12 @@ names(data_x) <- features[-unwanted]
 data_x_filtered <- select(data_x, dplyr::contains("mean()", ignore.case = FALSE), dplyr::contains("std"))
 
 # 1.2. Merge x and y
-data <- cbind(data_x_filtered, data_y_labeled)
+data <- cbind(data_y_labeled, data_subject) %>%
+  cbind(data_x_filtered)
 
-# 5. Show table
+## 5. Show table
 summary_data <- data %>% 
-  group_by(activity) %>%
+ group_by(activity, subject) %>%
   summarize_all(funs(mean))
 
 # Output file
